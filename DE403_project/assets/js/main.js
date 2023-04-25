46161191ebf8a7726cf85a9b59bb91ef5b10be01
Eshller <20B030016@iitb.ac.in>
2023-04-25 19:19:59 +0530

    navbar

--- DE403_project/assets/js/main.js
+++ DE403_project/assets/js/main.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
    * Sticky header on scroll
    */
   const selectHeader = document.querySelector('#header');
+
   if (selectHeader) {
     document.addEventListener('scroll', () => {
       window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
